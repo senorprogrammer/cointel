2e05c9990fb3d01bebafbd4190291eaf6d44170c
Chris Cummer <chriscummer@me.com>
2017-09-01 19:35:45 -0700

    Command-line args to specify the output type
    Examples:
    
            go run cointel.go
            go run cointel.go --output=table
            go run cointel.go --output=json
    
    Defaults to 'table'.

--- cointel.go
+++ cointel.go
@@ -3,6 +3,8 @@ package main
 import (
 	"github.com/senorprogrammer/cointel/modules"
 
+	"flag"
+	"fmt"
 	"time"
 )
 
@@ -11,19 +13,23 @@ const FiveMinutes = 300 * time.Second
 const FifteenMinutes = (15 * 60) * time.Second
 
 func main() {
+	outputPtr := flag.String("output", "table", "Either 'table' or 'json'")
+	flag.Parse()
+
 	modules.MakeTermination()
 	modules.MakeClearTerminal()
 
-	// Each exchange needs it's own client that handles how to load up their own
-	// CurrencyContainer with applicable data
 	coinbaseClient := modules.NewCoinbaseClient()
 
 	for {
 		coinbaseClient.Refresh()
 
 		// Output the results
-		modules.Table(&coinbaseClient.Container)
-		// fmt.Println(modules.Json(&coinbaseClient.Container))
+		if *outputPtr == "json" {
+			fmt.Println(modules.Json(&coinbaseClient.Container))
+		} else {
+			modules.Table(&coinbaseClient.Container)
+		}
 
 		time.Sleep(OneMinute)
 	}
